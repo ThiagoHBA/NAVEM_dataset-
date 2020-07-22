07282d01379e9c6111df78c85f81367a6f8d5c8d
Thiago Henrique <thiagohhab@gmail.com>
2020-07-22 08:31:52 -0300

    calculo da velocidade MPU6050

--- ExperimentoVelocidade_6050.py
+++ ExperimentoVelocidade_6050.py
@@ -18,36 +18,46 @@ GyroY = []
 GyroZ = []
 
 tempos = []
+roll = []
+pitch = []
+yaw = []
 
 gravityForce = 9.80665
-
+past_vel = []
+media_acc = 0
 quaternions_count = []
 velocidades = []
 
-pasta = "C:/Users/thiag/Desktop/Velocidade exps/MPU6050/Experimentos_delay_baixo/Experimentos_aaWorld/"
-arquivo = "Experimento 5.txt"
+pasta = "C:/Users/thiag/Desktop/Experimentos/Experimentos_acc_linear/Exp1/"
+arquivo = "sensores.json"
 sense_txt = open(pasta + arquivo,"r")
 sensor = json.load(sense_txt) 
 
 class Dados:
     def obter_acc():
-        for Acc in sensor["Dados"]:
-            AccX.append(abs(Acc["AccX"]))
-##            AccY.append(Acc["AccY"])
-##            AccZ.append(Acc["AccZ"])
-##        for i in range(len(AccX)):
-##            AccCount.append(np.array([AccX[i],AccY[i],AccZ[i]]))
+        for Acc in sensor["dados"]:
+            AccX.append(Acc["AccX"])
+            AccY.append(Acc["AccY"])
+            AccZ.append(Acc["AccZ"])
+        for i in range(len(AccX)):
+            AccCount.append(np.array([AccX[i],AccY[i],AccZ[i]]))
         
     def obter_gyr():
-        for Gyr in sensor["Dados"]:
+        for Gyr in sensor["dados"]:
             GyroX.append(Gyr["GyrX"])
             GyroY.append(Gyr["GyrY"])
             GyroZ.append(Gyr["GyrZ"])
+
+    def obter_rpy():
+        for rpy in sensor["dados"]:
+            roll.append(rpy["Row"])
+            pitch.append(rpy["Pitch"])
+            yaw.append(rpy["Yaw"])
         
 
     def obter_time():
-        for t in sensor["Dados"]:
-            tempos.append(t["time_usec"])
+        for t in sensor["dados"]:
+            tempos.append(t["time_sec"])
     
             
     def product(lista): 
@@ -72,51 +82,79 @@ class Dados:
         return val_int
 
 
+    def integration_trapeze(a, b, f, delta_t):
+##        f_a = f(a)
+##        f_b = f(b)
+        f_a = a
+        f_b = b
+        val_int = (f_a+f_b)/2.
+        val_int *= delta_t
+
+        return val_int
+
     def obter_velocidade():
-        for i in range(0,len(tempos),5):
-            f = Dados.lagrange(tempos[i : i+5], AccX[i : i+5])
-            for j in range(4, 0, -1):
-                velocidades.append(Dados.integration_simpson(tempos[i], tempos[i+j], f, 100))
-            
-##        velocidades.append(Dados.integration_simpson(tempos[0], tempos[5], f, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[0], tempos[4], f, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[0], tempos[3], f, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[0], tempos[2], f, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[0], tempos[1], f, 100))
-##        f1 = Dados.lagrange(tempos[60 : 66], AccX[60 : 66])
-##        velocidades.append(Dados.integration_simpson(tempos[60], tempos[65], f1, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[60], tempos[64], f1, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[60], tempos[63], f1, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[60], tempos[62], f1, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[60], tempos[61], f1, 100))
-##        f2 = Dados.lagrange(tempos[120 : 125], AccX[120 : 125])
-##        velocidades.append(Dados.integration_simpson(tempos[120], tempos[124], f2, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[120], tempos[123], f2, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[120], tempos[122], f2, 100))
-##        velocidades.append(Dados.integration_simpson(tempos[120], tempos[121], f2, 100))
-            
-##   [0.009276723563242089, 0.0069568121411608405, 0.0064906208558603, 0.004998603914599093]
+##        f = Dados.lagrange(tempos[8:16],AccX[8:16])
+##        for i in range(7):
+##            delta_t = tempos[(8+1)+i] - tempos[8+i]
+##            velocidades.append(Dados.integration_trapeze(tempos[(8+1)+i], tempos[8+i], f, delta_t))
+        for j in range(0,len(tempos[306:411]), 1):
+            f = Dados.lagrange(tempos[j:j+2], AccX[j:j+2])
+            delta_t = tempos[306+j+1] - tempos[306+j] #calculo o intervalo de tempo ( altura do trapézio ).
+            velocidades.append(Dados.integration_trapeze(AccX[306+j],AccX[306+j+1], f, delta_t)) #calcula a área de um trapézio e joga no array.
+
+
+'''
+EXP5:
+
+Pico 1: [7:12]
+vale 1: [12:24]
+Pico 2: [24:30]
+vale 2: [30:38]
+Pico 3: [38:46]
+Vale 3: [46:56]
+Pico 4: [56:62]
+Vale 4: [62:72]
+Pico 5: [72:81]
+Vale 5: [81:89]
+Pico 6: [89:98]
+'''
+
 Dados.obter_acc()
-##Dados.obter_gyr()
+Dados.obter_gyr()
+Dados.obter_rpy()
 Dados.obter_time()
 
 AccX = np.array(AccX).astype(float)
-##AccY = np.array(AccY).astype(float)
-##AccZ = np.array(AccZ).astype(float)
-##AccCount = np.array(AccCount).astype(float)
+AccY = np.array(AccY).astype(float)
+AccZ = np.array(AccZ).astype(float)
+AccCount = np.array(AccCount).astype(float)
 
-##GyroX = np.array(AccX).astype(float)
-##GyroY = np.array(AccY).astype(float)
-##GyroZ  = np.array(AccZ).astype(float)
+GyroX = np.array(AccX).astype(float)
+GyroY = np.array(AccY).astype(float)
+GyroZ  = np.array(AccZ).astype(float)
+
+roll = np.array(roll).astype(float)
+pitch = np.array(pitch).astype(float)
+yaw  = np.array(yaw).astype(float)
 
 tempos = np.array(tempos).astype(float)
 
 Dados.obter_velocidade()
+print(velocidades)
+print(sum(velocidades))
+##print("Velocidade média: ",sum(velocidades))
 
-##print("Velocidade media: ", np.mean(velocidades))
-print("Velocidade média: ",np.mean(velocidades))
+plt.plot(tempos,AccX, marker = "o")
+##plt.plot(tempos,AccY, color = "red")
+##fig, axs = plt.subplots(2)
+##axs[0].plot(tempos,AccX, marker = "o")
+##axs[1].plot(velocidades, marker = "o")
+##plt.plot(tempos[55:65],AccX[55:65], color = "red")
+##plt.plot(tempos[10:14], AccX[10:14], color = "black", marker = "o")
+##plt.plot(tempos[28:32],AccX[28:32], color = "black", marker = "o")
+##plt.plot(tempos[43:48],AccX[43:48], color = "black", marker = "o")
+##plt.plot(tempos[60:65],AccX[60:65], color = "black", marker = "o")
 
-plt.plot(tempos[1:125],AccX[1:125])
 plt.show()
 
 
